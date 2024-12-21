f12619ee55c96852d512638fec1b456c28a8226f
IamSila <silamulingi@gmail.com>
2024-12-21 16:58:10 +0300

    More updates on tests

--- findmykid/tests.py
+++ findmykid/tests.py
@@ -1,7 +1,9 @@
 from django.test import TestCase
+from .models import Report
+from django.core.files import File
 
 # Create your tests here.
-class SimpleTests(TestCase):
+class SimpleStatusCodesTests(TestCase):
     def test_report_page_status_code(self):
         responseReport = self.client.get('/report/')
         self.assertEqual(responseReport.status_code, 200)
@@ -11,3 +13,17 @@ class SimpleTests(TestCase):
     def test_base_page_status_code(self):
         responseBase = self.client.get('/base/')
         self.assertEqual(responseBase.status_code, 200)
+
+
+class ReportModelTest(TestCase):
+    def setUp(self):
+        Report.objects.create(firstName="Sila", age=5, height=1)
+         
+    def test_firstName_content(self):
+        report = Report.objects.get(id=1)
+        expectedFirstName = f'{report.firstName}'
+        self.assertEqual(expectedFirstName, "Sila")
+    def test_Height_content(self):
+        report = Report.objects.get(id=1)
+        expected_height = f"{report.height}"
+        self.assertEqual = (expected_height, 5)
