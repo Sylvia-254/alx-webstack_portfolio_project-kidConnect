3b8a1a7d5fe79be63ae7e3ee3c364705ce39390d
IamSila <silamulingi@gmail.com>
2024-12-23 20:34:34 +0300

    merging

--- findmykid/urls.py
+++ findmykid/urls.py
@@ -3,6 +3,7 @@ from .views import Base, Report
 from . import views
 
 urlpatterns = [
+    path('', )
     path('base/', Base.as_view(), name='base'),
     path('report/', views.report, name='report'),
     path('reported/', views.reported, name='reportedChildren'),
