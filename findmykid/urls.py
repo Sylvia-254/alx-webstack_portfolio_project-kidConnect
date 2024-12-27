bbcd6551d38ad0442fa26ac7e50ef4dce50e42de
IamSila <silamulingi@gmail.com>
2024-12-27 14:37:04 +0300

    registration, and login done

--- findmykid/urls.py
+++ findmykid/urls.py
@@ -5,9 +5,10 @@ from . import views
 urlpatterns = [
     path('', IndexView.as_view(), name="home"),
     path('register/', views.registerPage, name="register"),
+    path('login/', views.LoginView, name='login'),
     path('base/', Base.as_view(), name='base'),
     path('report/', views.report, name='report'),
     path('reported/', views.reported, name='reportedChildren'),
     path('search/', views.search, name="search"),
-
+    path('logout/', views.logoutView, name="logout"),
 ]
--- findmykid/views.py
+++ findmykid/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
 from django.contrib.auth.forms import UserCreationForm
-
-
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 #import models here
 from .models import Report
 
@@ -14,23 +16,67 @@ class IndexView(TemplateView):
     template_name = 'index.html'
 
 def registerPage(request):
-    form = UserCreationForm()
-    if request.method == "POST":
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
+    if request.method == 'POST':
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        user_data_has_error = False
+
+        # checking if username exists
+        if User.objects.filter(username=username).exists():
+            user_data_has_error = True
+            messages.error(request, "Username already exists")
+        # checking if the email exists
+        if User.objects.filter(username=username).exists():
+            user_data_has_error = True
+            messages.error(request, "Email already exists")
+        #checking the password length
+        if len(password) < 5:
+            messages.error(request, "Password must be atleast 5 characters")
+
+        if user_data_has_error:
+            return redirect('register')
+        else:
+            new_user = User.objects.create_user(
+                first_name=first_name,
+                last_name=last_name,
+                username=username,
+                email=email,
+                password=password,
+            )
+            messages.success(request, "Account created successfully, login now")
+            #return rediret('login')
+    return render(request, 'register.html')
 
 
+def LoginView(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+    
+        user = authenticate(request, username=username, password=password)
 
-    context = {'form':form}
-    return render(request, 'register.html', context)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, "Invalid username or password")
+            return redirect('login')
+    return render(request, 'login.html')
 
 class Base(TemplateView):
     template_name = 'base1.html'
 
+
+def logoutView(request):
+    logout(request)
+    return redirect('home')
 # class Report(TemplateView):
 #     template_name = 'report.html'
-
+@login_required # restrict this page to authenticated users
 def report(request):
     """the report page and its logic is here"""
     if request.method == 'POST':
@@ -53,7 +99,7 @@ def report(request):
         
         newReport.save()
     return render(request, 'report.html')
-
+@login_required #restrict this page to authenticated users
 def reported(request):
     """logic for retrieving reported children from the database"""
     context = {}
@@ -62,6 +108,7 @@ def reported(request):
     return render(request, 'reportedChildren.html', context)
 
 # reportedChildren search
+@login_required #restrict this page to restricted users
 def search(request):
     """logic for the search bar and button"""
     if request.method == 'GET':
